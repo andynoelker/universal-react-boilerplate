fc3582a50c7b883deabef7a4688579756e7c38e9
Andy Noelker <andynoelker@gmail.com>
2018-04-08 14:09:53 -0500

    Add sample React Router setup

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import NoMatch from './NoMatch';
 
 class App extends React.Component {
   render() {
-    return <div>App is up and running</div>;
+    return (<div>
+      <p>App is up and running</p>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>);
   }
 }
 
